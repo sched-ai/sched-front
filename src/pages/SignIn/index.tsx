0e1ebe5203c89b86b59d089c4d33a34aa5ffecef
Felipe Bittencourt <135668472+Felipembits@users.noreply.github.com>
2025-09-06 16:52:06 -0300

    feat: responsividade da página de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,14 +5,15 @@ import { Input } from "../../components/ui/input";
 export const SignIn = () => {
   return (
     <div
-      className="flex p-6 w-full min-h-screen bg-cover bg-center"
+      className="flex p-11 w-full min-h-screen bg-cover bg-center"
       style={{ backgroundImage: `url(${abstract})` }}
     >
-      <div className="flex flex-col justify-between w-full gap-4">
+      <div className="flex flex-col w-full gap-4">
         <div className="flex justify-between items-center mb-4">
-          <h1 className="text-2xl font-semibold text-white italic">SCHED</h1>
+          <h1 className="text-2xl lg:text-4xl font-semibold text-white italic">SCHED</h1>
           <Button
             variant="seccondary"
+            className="flex lg:hidden"
             onClick={() => {
               window.location.href = "/signup";
             }}
@@ -20,71 +21,90 @@ export const SignIn = () => {
             Cadastre-se
           </Button>
         </div>
-        <div className="w-full max-w-[510px] m-auto">
-          <form
-            className="space-y-6 px-6 py-8 lg:px-10 lg:py-14
-                        [background-clip:padding-box,border-box] 
-                        backdrop-blur-md bg-black/45"
-          >
-            <div className="text-start text-white">
-              <h1 className="lg:text-[40px] font-semibold leading-[1.2] text-2xl">
-                Acesse sua conta
-              </h1>
-            </div>
-
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-normal text-white mb-2"
-              >
-                Email
-              </label>
-              <Input
-                type="email"
-                id="email"
-                required
-                placeholder="scheapp@gmail.com"
-                className="text-white border-white"
-              />
-            </div>
-
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-normal text-white mb-2"
-              >
-                Senha
-              </label>
-              <div className="relative">
-                <Input
-                  type="password"
-                  id="password"
-                  required
-                  placeholder="Insira sua senha"
-                  className="text-white border-white"
-                />
-                <Button variant="link">Esqueci minha senha</Button>
-              </div>
-            </div>
-
-            <Button type="submit" variant="secondary" className="w-full font-semibold">
-              ENTRAR
-            </Button>
-
-            <div className="text-center">
-              <span className="text-white font-light">
-                Novo por aqui?{" "}
-              </span>
+        <div className="flex justify-between h-full">
+             <div className="lg:flex flex-col justify-end max-w-[640px] w-full hidden">
+              <h2 className="lg:text-4xl text-2xl font-light text-white">
+                Novo por aqui?
+              </h2>
+              <h2 className="lg:text-4xl font-semibold text-white max-w-[250px] mb-10 text-2xl">
+                Crie sua conta! 
+              </h2>
               <Button
-                variant="link"
+                variant="seccondary"
+                className="w-full max-w-[394px]"
                 onClick={() => {
                   window.location.href = "/signup";
                 }}
               >
-                Cadastre-se
+                CADASTRE-SE
               </Button>
             </div>
-          </form>
+            <div className="w-full max-w-[510px] m-auto">
+            <form
+                className="space-y-6 px-6 py-8 lg:px-10 lg:py-14
+                            [background-clip:padding-box,border-box] 
+                            backdrop-blur-md bg-black/45"
+            >
+                <div className="text-start text-white">
+                <h1 className="lg:text-[40px] font-semibold leading-[1.2] text-2xl">
+                    Acesse sua conta
+                </h1>
+                </div>
+
+                <div>
+                <label
+                    htmlFor="email"
+                    className="block text-sm font-normal text-white mb-2"
+                >
+                    Email
+                </label>
+                <Input
+                    type="email"
+                    id="email"
+                    required
+                    placeholder="scheapp@gmail.com"
+                    className="text-white border-white"
+                />
+                </div>
+
+                <div>
+                <label
+                    htmlFor="password"
+                    className="block text-sm font-normal text-white mb-2"
+                >
+                    Senha
+                </label>
+                <div className="relative">
+                    <Input
+                    type="password"
+                    id="password"
+                    required
+                    placeholder="Insira sua senha"
+                    className="text-white border-white"
+                    />
+                    <Button variant="link">Esqueci minha senha</Button>
+                </div>
+                </div>
+
+                <Button type="submit" variant="secondary" className="w-full font-semibold">
+                ENTRAR
+                </Button>
+
+                <div className="text-center">
+                <span className="text-white font-light">
+                    Novo por aqui?{" "}
+                </span>
+                <Button
+                    variant="link"
+                    onClick={() => {
+                    window.location.href = "/signup";
+                    }}
+                >
+                    Cadastre-se
+                </Button>
+                </div>
+            </form>
+            </div>
         </div>
       </div>
     </div>
