a65475260da54b1ae14142fad7c95f6f156e1197
felipembits <felipebittencourt@id.uff.br>
2025-09-10 15:09:15 -0300

    feat: iniciando home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
 import { FirstLogin } from './pages/FirstLogin'
+import { Home } from './pages/Home'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<SignIn />}  />
       <Route path="/signup" element={<SignUp />}  />
       <Route path="/firstLogin" element={<FirstLogin />}  />
+      <Route path="/home" element={<Home />}  />
      </Routes>
     </>
   )
