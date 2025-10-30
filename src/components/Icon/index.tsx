c202f171457c3f70ec83401011040747f89a7119
João Portela <joaoportelamadureira@gmail.com>
2025-10-30 14:49:18 -0300

    feat: novo ícone de empresa

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -589,6 +589,14 @@ export const svg_object = {
 				</clipPath>
 			</defs>
 		</>
+	),
+	enterprise: () => (
+		<>
+			<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 32 32">
+				<path fill="currentColor" d="M8 8h2v4H8zm0 6h2v4H8zm6-6h2v4h-2zm0 6h2v4h-2zm-6 6h2v4H8zm6 0h2v4h-2z" />
+				<path fill="currentColor" d="M30 14a2 2 0 0 0-2-2h-6V4a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v26h28ZM4 4h16v24H4Zm18 24V14h6v14Z" />
+			</svg>
+		</>
 	)
 };
 
